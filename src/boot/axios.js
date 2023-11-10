930ef22d4766c11de6513b9bf31a6e129210611e
Hamed Masafi <hamed.masafi@gmail.com>
2023-11-10 12:11:16 +0330

    /http/https/

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,7 @@ import axios from "axios";
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: "http://parke-api.li0.ir/api/" });
+const api = axios.create({ baseURL: "https://parke-api.li0.ir/api/" });
 
 export default boot(({ app }) => {
   // for use inside Vue files (Options API) through this.$axios and this.$api
